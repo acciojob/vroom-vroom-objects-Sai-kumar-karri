c55e33652f006a8be305eaba61e7f13b1bc75fc0
sai kumar karri <saikumarkarri950@gmail.com>
2025-08-28 13:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 // Complete the js code
-function Car(make, model) {}
+function Car (make,model){
+    if(typeof make == "string" && typeof model == "sting"){
+        throw new Error("make and model should be srting")
+    }
 
-function SportsCar(make, model, topSpeed) {}
+    this.make = make;
+    this.model = model;
+}
+    Car.prototype.getMakeModel = function(){
+        return `${this.make} ${this.model}`
+    }
+
+
+    function SportsCar(make,model,topSpeed){
+        this.make = make;
+        this.model = model;
+        this.topSpeed = topSpeed;
+
+
+    }
+
+    SportsCar.prototype = Object.create(Car.prototype)
+    SportsCar.prototype.constructor = SportsCar
+
+    SportsCar.prototype.getTopSpeed = function(){
+
+        return this.topSpeed
+
+    }
+
+    const car = new SportsCar("Ferrari", "Testarossa", 200);
+    console.log(car.getMakeModel())
+    console.log(car.getTopSpeed())
+
+
+
+
 
 // Do not change the code below
 window.Car = Car;
